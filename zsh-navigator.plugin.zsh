d4ebb9d396affeb12fd07d26683132bc9e9af743
Andre Ribeiro <andresmribeiro@gmail.com>
2024-11-17 22:51:11 +0000

    chore: vars

--- zsh-navigator.plugin.zsh
+++ zsh-navigator.plugin.zsh
@@ -9,9 +9,10 @@ fi
 export NAVIGATOR_MODE="${NAVIGATOR_MODE:-tmux}" # Options: "tmux" or "cd"
 export NAVIGATOR_BASE_DIR="${NAVIGATOR_BASE_DIR:-$HOME/github}" # Base directory
 export NAVIGATOR_STRUCTURE="${NAVIGATOR_STRUCTURE:-workspace}" # Options: "unsorted", "workspace", "workspace_src"
-export NAVIGATOR_KEYBIND_ENABLED="${NAVIGATOR_KEYBIND_ENABLED:-false}" # Enable keybinding? "true" or "false"
+export NAVIGATOR_KEYBIND_ENABLED="${NAVIGATOR_KEYBIND_ENABLED:-true}" # Enable keybinding? "true" or "false"
 export NAVIGATOR_KEYBIND="${NAVIGATOR_KEYBIND:-^f}" # Default keybinding (can be changed)
-export NAVIGATOR_ORIGINAL_PATH="${NAVIGATOR_ORIGINAL_PATH:-.}" # Default path to return to
+export NAVIGATOR_HOME_KEYBIND="${NAVIGATOR_KEYBIND:-^h}" # Default keybinding (can be changed)
+export NAVIGATOR_HOME_PATH="${NAVIGATOR_HOME_PATH:-.}" # Default path to return to
 
 navigator() {
     if [[ "$NAVIGATOR_MODE" == "cd" ]]; then
@@ -23,5 +24,5 @@ navigator() {
 
 if [[ "$NAVIGATOR_KEYBIND_ENABLED" == true ]]; then
     bindkey -s "$NAVIGATOR_KEYBIND" "navigator\n"
-    bindkey -s "^h" "cd $NAVIGATOR_ORIGINAL_PATH\n"
+    bindkey -s "$NAVIGATOR_HOME_KEYBIND" "cd $NAVIGATOR_ORIGINAL_PATH\n"
 fi
